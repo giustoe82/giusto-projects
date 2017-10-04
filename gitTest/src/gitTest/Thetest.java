cb7a6adc2350f75ab29b20f2d00111b02def4bb1
marcog <marcog@192.168.20.73>
2017-10-04 13:07:21 +0200

    test

--- gitTest/src/gitTest/Thetest.java
+++ gitTest/src/gitTest/Thetest.java
@@ -5,7 +5,7 @@ public class Thetest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("no test");
+		System.out.println("no test2");
 
 	}
 
