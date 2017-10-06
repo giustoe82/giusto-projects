bd620d7e6334a6d48e78c0bc64902730f8306614
marcog <marcog@192.168.20.47>
2017-10-06 14:51:03 +0200

    fre 06/10 14.50

--- Black Jack try/src/GameRunner.java
+++ Black Jack try/src/GameRunner.java
@@ -5,6 +5,20 @@ public class GameRunner {
 
 	public static void main(String[] args) {
 		
+		  /// This part aims to get multi players number & their names;
+        Player player = new Player();
+        Player [] players = player.multiPlayers();
+
+        int numberOfPlayers = players.length;
+
+//        System.out.println(numberOfPlayers);
+
+        for (int i = 0; i < numberOfPlayers; i++) {
+            String s = players[i].getName();
+            System.out.println(s);
+        }
+        //// Multi players part ends here. When we submit, Print lines should be deleted.
+		
 		boolean playAgain = true;
 		
 		Scanner play = new Scanner(System.in);
@@ -17,27 +31,30 @@ public class GameRunner {
 		Deck theDeck = new Deck(7, true);
 		
 		// initialize player objects
-		Player one = new Player("Eddy");
+		//Player one = new Player("Eddy");
 		Player dealer = new Player ("Dealer");
 		
 		int money = 100;
 		int bet;
 		int left;
 		
+		while (playAgain = true) {	
+			
 		
+	for (int i = 0; i < numberOfPlayers; i++) {
 		
-		while (playAgain = true) {
-		
-		one.addCard(theDeck.dealNextCard());
+		players[i].addCard(theDeck.dealNextCard());
+		players[i].addCard(theDeck.dealNextCard());
+	}
+	
 		dealer.addCard(theDeck.dealNextCard());
-		one.addCard(theDeck.dealNextCard());
 		dealer.addCard(theDeck.dealNextCard());
-		System.out.println("Your actual stack is: " +money);
+		/*System.out.println("Your actual stack is: " +money);
 		System.out.println("How much do you want to bet?");
 		bet = dollar.nextInt();
 		System.out.print("Your bet is: "+bet +". ");
 		left = money - bet;
-		System.out.println("Money left: "+left);
+		System.out.println("Money left: "+left);*/
 		
 		
 		// flags for when each player is finished hitting
@@ -45,14 +62,20 @@ public class GameRunner {
 				boolean dealerDone = false;
 				boolean blackJack= false;
 				String ans;
-		
+				
+				System.out.println("Cards are dealt\n");
+				for (int i = 0; i < numberOfPlayers; i++) {
 		// print initial hands
-		System.out.println("Cards are dealt\n");
-		one.printHand(true);
-		System.out.println("total for player: " +one.getHandSum());
-		dealer.printHand(false);
+		
+		players[i].printHand(players[i].getName(), true);
+		System.out.printf(" total for  %s is %s\n",  players[i].getName(),players[i].getSum());
+				}
+		dealer.printHand(dealer.getName(), false);
+		
 		System.out.println("\n");
-		if (one.getHandSum() == 21) {
+		
+		for (int i = 0; i < numberOfPlayers; i++) {
+		if (players[i].getSum() == 21) {
 			System.out.println("***************");
 			System.out.println("***BLACKJACK***");
 			System.out.println("***************");
@@ -60,14 +83,14 @@ public class GameRunner {
 			dealerDone = true;
 			
 			blackJack = true;
-			int bj=22;
-			bj=dealer.getHandSum();
-			System.out.println("You win " +3*bet);
-			money = left + 3*bet;
+			//int bj=22;
+			//bj=dealer.getSum();
+			//System.out.println("You win " +3*bet);
+			//money = left + 3*bet;
 		
 			
-			
-		}
+		}	
+		
 		
 		
 		
@@ -84,16 +107,16 @@ public class GameRunner {
 				if (ans.compareToIgnoreCase("H") == 0) {
 					
 					//add next card in the deck and store whether player is busted
-					oneDone = !one.addCard(theDeck.dealNextCard());
+					oneDone = !players[i].addCard(theDeck.dealNextCard());
 					
-					one.printHand(true);
-					System.out.println("total for player: " +one.getHandSum());
+					players[i].printHand(players[i].getName(),true);
+					System.out.printf(" total for  %s is %s\n",  players[i].getName(),players[i].getSum());
 					
-					if (one.getHandSum() > 21) {
+					if (players[i].getSum() > 21) {
 						System.out.println("<<<<<<BUSTED>>>>>>");
 						dealerDone = true;
 					}
-					if (one.getHandSum() == 21) {
+					if (players[i].getSum() == 21) {
 						System.out.println("Congratulations!!! 21");
 						oneDone = true;
 					}
@@ -111,7 +134,8 @@ public class GameRunner {
 			System.out.println();
 			
 		}
-		dealer.printHand(true);
+		}
+		dealer.printHand(dealer.getName(),true);
 		
 		while(!dealerDone) {
 			
@@ -119,27 +143,29 @@ public class GameRunner {
 			if (!dealerDone) {
 				
 				
-				if (dealer.getHandSum()<one.getHandSum() && (one.getHandSum()>=17 && one.getHandSum()<22)) {
+		/*		if (dealer.getSum()<players[i].getSum() && (players[i].getSum()>=17 && players[i].getSum()<22)) {
 					System.out.println("\n");
 					System.out.println("The dealer hits\n");
 					dealerDone = !dealer.addCard(theDeck.dealNextCard());
 					
-					dealer.printHand(true);
-					System.out.println("total for dealer: " +dealer.getHandSum());
+					dealer.printHand(dealer.getName(),true);
+					System.out.println("total for dealer: " +dealer.getSum());
 					
-					if (dealer.getHandSum() < 17) {
+					*/
+				
+					if (dealer.getSum() < 17) {
 						System.out.println("\n");
 						System.out.println("The dealer hits\n");
 						dealerDone = !dealer.addCard(theDeck.dealNextCard());
-						dealer.printHand(true);
-						System.out.println("total: " +dealer.getHandSum());
+						dealer.printHand(dealer.getName(),true);
+						System.out.println("total for dealer: " +dealer.getSum());
 						
 					}
 				} else {
 					System.out.println("\n");
 					System.out.println("The dealer stays\n");
 					dealerDone = true;
-					System.out.println("total for dealer: " +dealer.getHandSum());
+					System.out.println("total for dealer: " +dealer.getSum());
 				}
 			}
 			System.out.println();
@@ -148,42 +174,45 @@ public class GameRunner {
 	
 		
 		//print final hands
-		one.printHand(true);
-		dealer.printHand(true);
+		dealer.printHand(dealer.getName(),true);
+		int dealerSum = dealer.getSum();
 		
-		int oneSum = one.getHandSum();
-		int dealerSum = dealer.getHandSum();
+		for (int i = 0; i < numberOfPlayers; i++) {
+		players[i].printHand(players[i].getName(),true);
+		int oneSum = players[i].getSum();
+		
+		
 		
 	
 		
 		if ((oneSum > dealerSum && oneSum <= 21 || dealerSum > 21) ) {
-			System.out.println("Total for player: " + one.getHandSum());
-			System.out.println("You win " +2*bet );
+			System.out.printf(" total for  %s is %s\n",  players[i].getName(),players[i].getSum());
+			System.out.printf("%s wins ", players[i].getName() );
 			
 			
-			money = left + 2*bet;
+			//money = left + 2*bet;
 			
-			System.out.println("Your actual stack is: " +money);
+			//System.out.println("Your actual stack is: " +money);
 		
 		
 			
 		
 			}
-		if (blackJack == true) {
+		/*if (blackJack == true) {
 			System.out.println("You win " +3*bet);
 			money = left + 3*bet;
 			System.out.println("Your actual stack is: " +money);
-		}
+		}*/
 			
 			
 			
 		 else if (oneSum < dealerSum && dealerSum <= 21 || oneSum > 21){
-			System.out.println("Total for dealer: " + dealer.getHandSum());
+			System.out.println("Total for dealer: " + dealer.getSum());
 			System.out.println("Dealer wins ");
-			money = money - bet;
-			System.out.println("Your actual stack is: " +money);
+			//money = money - bet;
+			//System.out.println("Your actual stack is: " +money);
 		}
-		
+		}
 		System.out.println();
 		System.out.println();
 		System.out.println("Play again? y/n");
@@ -196,8 +225,10 @@ public class GameRunner {
 		if (ans2.compareToIgnoreCase("Y") == 0) {
 			playAgain = true;
 			replay = false;
-			one.emptyHand();
-			dealer.emptyHand();
+			for (int i = 0; i < numberOfPlayers; i++) {
+			players[i].empty();
+			}
+			dealer.empty();
 			bet = 0;
 		
 			
@@ -206,11 +237,11 @@ public class GameRunner {
 			sc.close();
 			play.close();
 			dollar.close();
-			System.out.println("Thank you for playing");
+			System.out.println("Thank you for playing. Bye Bye!!!");
 			
 			System.exit(0);
 		} else {
-			System.err.println("input wrong, try again ...push a button ");
+			System.err.println("input wrong, try again ");
 			replay = true;
 			ans2 = play.next();
 		}
@@ -220,4 +251,5 @@ public class GameRunner {
 	}
 
 }
-}
+
+	
